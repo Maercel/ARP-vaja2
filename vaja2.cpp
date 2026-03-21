ca5950bbc1f807a525fd4256a7540744aa901f1a
Maercel <marcel.majhenic@gmail.com>
2026-03-21 22:20:02 +0100

    Added main function, ansi_codes and libraries

---
+++ vaja2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+namespace ansi_codes {
+	constexpr const char* color_reset = "\033[0m";
+	constexpr const char* color_red = "\033[31m";
+	constexpr const char* color_green = "\033[32m";
+	constexpr const char* color_yellow = "\033[33m";
+	constexpr const char* color_blue = "\033[34m";
+
+	constexpr const char* console_clear_line = "\33[2K";
+}
+
+int main(int argc, char** argv) 
+{
+	
+
+	return 0;
+}
